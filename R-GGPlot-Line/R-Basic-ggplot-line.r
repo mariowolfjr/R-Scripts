80302404be48ad06cd6a2fe6441ede9adf3221b5
mariowolfjr <51292743+mariowolfjr@users.noreply.github.com>
2023-03-09 14:38:16 -0300

    Add files via upload

---
+++ R-GGPlot-Line/R-Basic-ggplot-line.r
@@ -0,0 +1,30 @@
+# - Date Time Handling
+# - Padding timeseries by hour
+# - Converting NA to 0
+#
+
+#https://statisticsglobe.com/insert-rows-for-missing-dates-in-r
+#---------------------------------------------------------------
+install.packages("padr")                           # Install & load padr package
+
+library("padr")
+library("ggplot2")
+
+df.Data <- read.csv("C:/Users/mwolf/OneDrive - COGNYTE/Documents/COGNYTE/INSTALACAO/2022/4.RAPANUI/28.CALL-PROFILE/temp.csv")
+df.Data$DateTime2 <- as.POSIXct(df.Data$DateTime, tz = "", '%Y-%m-%d %H:%M')
+
+
+head(df.Data)
+
+df.Data$DateTime <- NULL
+df.Data$Time     <- NULL
+df.DataPad <- pad(df.Data, start_val = as.POSIXct("2023-02-01 00:00:00", tz = "", '%Y-%m-%d %H:%M'))
+
+head(df.DataPad)
+
+
+# Convert NA to 0
+df.DataPad[is.na(df.DataPad)] = 0
+
+
+ggplot(data=df.DataPad, aes(DateTime2, Calls)) + geom_line() 
