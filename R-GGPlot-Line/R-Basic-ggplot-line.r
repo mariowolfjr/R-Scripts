02b65fc7b526c208e3dd801f72d2ab1c5d781d5c
Mario Wolf Jr <mariowolfjr@gmail.com>
2023-03-31 15:39:47 -0300

    Credentials test.

--- R-GGPlot-Line/R-Basic-ggplot-line.r
+++ R-GGPlot-Line/R-Basic-ggplot-line.r
@@ -1,7 +1,7 @@
 # - Date Time Handling
 # - Padding timeseries by hour
 # - Converting NA to 0
-#
+# - teste
 
 #https://statisticsglobe.com/insert-rows-for-missing-dates-in-r
 #---------------------------------------------------------------
@@ -9,18 +9,27 @@
 library("padr")
 library("ggplot2")
 
-df.Data <- read.csv("28.CALL-PROFILE/temp.csv")
-df.Data$DateTime2 <- as.POSIXct(df.Data$DateTime, tz = "", '%Y-%m-%d %H:%M')
+df.Data <- read.csv("R-GGPlot-Line/Calls_2.csv")
 
+df.Data$DateTime2 <- as.POSIXct(df.Data$DateTime, tz = "", '%Y-%m-%d %H:%M')
 head(df.Data)
 
 df.Data$DateTime <- NULL
 df.Data$Time     <- NULL
-df.DataPad <- pad(df.Data, start_val = as.POSIXct("2023-02-01 00:00:00", tz = "", '%Y-%m-%d %H:%M'))
+df.DataPad <- pad(df.Data, start_val = as.POSIXct("2023-03-01 00:00:00", tz = "", '%Y-%m-%d %H:%M'))
 
 head(df.DataPad)
 
+str(df.DataPad)
 
 # Convert NA to 0
 df.DataPad[is.na(df.DataPad)] = 0
-ggplot(data=df.DataPad, aes(DateTime2, Calls)) + geom_line() 
+
+#df.DataPad$Date <- as.Date(df.DataPad$DateTime2)
+
+
+ggplot(data=df.DataPad, aes(DateTime2, Value)) + geom_line() +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) +
+  scale_x_datetime(date_breaks = "day" , date_labels = "%Y-%m-%d")
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # R-Scripts
 R Scripts for General Purpose
+A line I wrote on my local computer  
